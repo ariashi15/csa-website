6685a59641d52da2beac107ea76d70b2ac30ea25
Aria Shi <ariashi15@gmail.com>
2026-09-16 22:43:20 +0800

    footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,24 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer__heading">Northwestern CSA</div>
-            <a
-                href="https://www.instagram.com/northwesterncsa"
-                target="_blank"
-                rel="noreferrer"
-                className="footer__link"
-            >
-                <img src="src/assets/images/instagram.svg" className="footer__icon"/>
-                @northwesterncsa
-            </a>
-            <a 
-                href="mailto:csa@u.northwestern.edu"
-                className="footer__link"
-            >
-                <img src="src/assets/images/mail.svg" className="footer__icon"/>
-                csa@u.northwestern.edu
-            </a>
+            <div className="footer__links">
+                <a
+                    href="https://www.instagram.com/northwesterncsa"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="footer__link"
+                >
+                    <img src="src/assets/images/instagram.svg" className="footer__icon"/>
+                    northwesterncsa
+                </a>
+                <a 
+                    href="mailto:csa@u.northwestern.edu"
+                    className="footer__link"
+                >
+                    <img src="src/assets/images/mail.svg" className="footer__icon"/>
+                    csa@u.northwestern.edu
+                </a>
+            </div>
         </div>
     );
 }
