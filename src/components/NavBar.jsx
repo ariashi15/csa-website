acaa7c1ba937415323f5d066529ebc94d8373bc2
Aria Shi <ariashi15@gmail.com>
2026-09-16 21:55:23 +0800

    add home link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,6 +9,7 @@ function NavBar() {
         Northwestern CSA
       </NavLink>
       <div className="navbar__links">
+        <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/our-team">Our Team</NavLink>
         <NavLink to="/events">Events</NavLink>
