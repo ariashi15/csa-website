907fd1b959746a29efcace296ef8c0c7b66f37bf
Alicia Li <aliciay.li9@gmail.com>
2026-09-18 01:28:48 -0700

    add past events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,9 +1,36 @@
+import SectionHeader from "../components/SectionHeader";
+import EventCard from "../components/EventCard";
+
+const events = [
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+  { tag: "CSA General", title: "Mid-Autumn Festival", date: "October 2025" },
+];
+
 function Events() {
   return (
-    <>
-      <div>Events</div>
-    </>
+    <section>
+      <SectionHeader title="Past Events" subtitle="What's Happening" />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gap: "40px",
+          margin: "40px 0",
+        }}
+      >
+        {events.map((event, index) => (
+          <EventCard key={index} {...event} />
+        ))}
+      </div>
+    </section>
   );
 }
 
-export default Events
+export default Events;
