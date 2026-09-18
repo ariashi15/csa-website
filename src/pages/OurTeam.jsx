9eddbf53fd066e826cb6f0d348d2ea12a8087d76
Aria Shi <ariashi15@gmail.com>
2026-09-18 15:44:29 +0800

    exec cards

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,7 +1,51 @@
+import SectionHeader from "../components/SectionHeader";
+import emptyImage from "../assets/images/empty-image.png";
+import execBoard from "../data/execBoard26-27.json";
+import "./OurTeam.css";
+
+// all img file names should be in the format firstlast.jpg
+const imageUrls = import.meta.glob("../assets/images/*.jpg", {
+  eager: true,
+  import: "default",
+  query: "?url",
+})
+
+function ExecCard({ name, position, imgUrl }) {
+  return (
+    <div className="exec-card">
+      <img src={imgUrl || emptyImage} />
+      <div className="exec-card__footer">
+        <div className="exec-card__name">{name}</div>
+        <div className="exec-card__position">{position}</div>
+      </div>
+    </div>
+  )
+}
+
+function getImgUrl(name) {
+  const fileName = name.toLowerCase().replace(/\s/g, '')
+  return imageUrls[`../assets/images/${fileName}.jpg`] || emptyImage
+}
+
 function OurTeam() {
   return (
     <>
-      <div>Our Team</div>
+      <SectionHeader title="Our Team" subtitle="Exec Board 2026-2027" />
+      <div className="exec-card__container">
+        {Object.entries(execBoard.cabinet).map(([position, member], index) => (
+          <ExecCard key={index} name={member} position={position} imgUrl={getImgUrl(member)} />
+        ))}
+      </div>
+      <div className="exec-card__container">
+        {Object.entries(execBoard.cabinet).map(([position, member], index) => (
+          <ExecCard key={index} name={member} position={position} imgUrl={getImgUrl(member)} />
+        ))}
+      </div>
+      <div className="exec-card__container">
+        {Object.entries(execBoard.cabinet).map(([position, member], index) => (
+          <ExecCard key={index} name={member} position={position} imgUrl={getImgUrl(member)} />
+        ))}
+      </div>
     </>
   );
 }
